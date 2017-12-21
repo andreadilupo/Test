bc6ea7be65c1c88af089bb1fec437ebfcde85fce
AndreaDilupo <andrea.dilupo@gmail.com>
2017-12-21 11:50:35 +0100

    bottone

--- Soluzione1Test/Progetto1Test/MainWindow.xaml.cs
+++ Soluzione1Test/Progetto1Test/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Progetto1Test
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
